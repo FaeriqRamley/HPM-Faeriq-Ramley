1e013ff09e9f7bdd22d841ff4e2db41da9fe5aa8
faeriqramley <mohamad.faeriq.ramley@gmail.com>
2021-11-23 17:01:05 +0800

    login page setup

--- frontend/src/components/NavbarComponent.js
+++ frontend/src/components/NavbarComponent.js
@@ -3,24 +3,24 @@ import {Link} from 'react-router-dom';
 
 function NavbarComponent() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand">Hendricks Corp</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand">Hendricks Corp</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <Link class='nav-link' to='/'>Home</Link>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className='nav-link' to='/'>Home</Link>
                         </li>
                         <li class="nav-item">
-                            <Link class='nav-link' to='/dashboard'>Dashboard</Link>
+                            <Link className='nav-link' to='/dashboard'>Dashboard</Link>
                         </li>
                     </ul>
-                    <ul class='navbar-nav'>
-                        <li class='nav-item'>
-                            <Link class='nav-link' to='/login'>Login</Link>
+                    <ul className='navbar-nav'>
+                        <li className='nav-item'>
+                            <Link className='nav-link' to='/login'>Login</Link>
                         </li>
                     </ul>
                 </div>
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React,{useState} from 'react';
+import {Col, Container, Form, Row,Button} from 'react-bootstrap';
 
 function LoginPage() {
+    const [login,toggleLogin] = useState(false);
+    
+    const handleToggle = (e) => {
+        e.preventDefault();
+        toggleLogin(!login);
+    }
+    
     return (
-        <div>
-            This is login page
-        </div>
+        <Container className='mt-3'>
+            <Row className='border py-2'>
+                <Col><h3 className='display-6'>{login ? 'Login' : 'Sign Up' }</h3></Col>
+                <Col className='d-flex justify-content-end align-items-center'>
+                    <Button className='btn btn-secondary h-80' onClick={handleToggle}>Click here to {login ? 'Sign Up' : 'Log In' }</Button>
+                </Col>
+                <Form>
+                    <Form.Group className='my-3' controlId='formUsername'>
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type='text' placeholder='Enter username'></Form.Control>
+                    </Form.Group>
+                    <Form.Group className='my-3' controlId='formPassword'>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type='password' placeholder='Enter password'></Form.Control>
+                    </Form.Group>
+                    { !login &&
+                        <>
+                            <Form.Group className='my-3' controlId='formApiKey'>
+                                <Form.Label>Api Key</Form.Label>
+                                <Form.Control type='text' placeholder='Enter Api Key'></Form.Control>
+                            </Form.Group>
+                            <Form.Group className='my-3' controlId='formApiToken'>
+                                <Form.Label>Api Server Token</Form.Label>
+                                <Form.Control type='text' placeholder='Enter Api Token'></Form.Control>
+                            </Form.Group>
+                        </>
+                    }
+                    <Form.Group className='d-flex justify-content-end'>
+                        <Button type='submit' className='btn btn-primary mb-3'>{login ? 'Login' : 'Sign Up' }</Button>
+                    </Form.Group>
+                </Form>
+            </Row>
+        </Container>
     )
 }
 
