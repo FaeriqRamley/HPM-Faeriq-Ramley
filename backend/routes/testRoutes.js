dcd3451c41419936969d852432e9f6e085dd802e
faeriqramley <mohamad.faeriq.ramley@gmail.com>
2021-11-25 18:10:25 +0800

    removed test api route, models and controllers

--- backend/routes/testRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const {test_post,test_get} = require('../controllers/testController');
-
-router.get('/test_get',test_get)
-router.post('/test_post',test_post)
-
-module.exports = router
